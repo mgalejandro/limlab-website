c677375555d6588dbfd8a851970fd91abae3709c
Hersh Bhargava <hershkbhargava@gmail.com>
2022-01-26 15:15:45 -0800

    Modernized site and updated Grunt architecture.
    Signed-off-by: Hersh Bhargava <hershkbhargava@gmail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+/**
+ * Installation:
+ * 1. Install Grunt CLI (`npm install -g grunt-cli`)
+ * 1. Install Grunt 0.4.0 and other dependencies (`npm install`)
+ *
+ * Build:
+ * Execute `grunt` from root directory of this directory (where Gruntfile.js is)
+ * To execute automatically after each change, execute `grunt --force default watch`
+ * To execute build followed by the test run, execute `grunt test`
+ *
+ * See http://gruntjs.com/getting-started for more information about Grunt
+ */
+module.exports = function (grunt) {
+	grunt.initConfig({
+			jade: {
+				compileAll: {
+					options: {
+						pretty: true,
+					},
+					files: [{
+						expand: true,
+						src: ["**/*.jade", "!**/_*.jade"], 
+						cwd: 'src/',
+						ext: ".html",
+						dest: 'dest/'
+					}]
+				}
+			},
+			
+			cssmin: {
+				main: {
+					expand: true,
+					cwd: 'src/css/',
+					src: ['*.css', '!*.min.css'],
+					dest: 'dest/css/',
+					extDot: 'last',
+					ext: '.min.css'
+				}
+			},
+
+			sass: {
+				dist: {
+					files: [{
+						src: "*.scss", 
+						cwd: "src/sass",
+						dest: "dest/css", 
+						expand: true,
+						ext: ".css"
+					}]
+				}
+			},
+
+			clean: {
+				dist: ['tmp']
+			},
+
+			connect: {
+				dev: {
+					options: {
+						port: 9000,
+						hostname: "0.0.0.0",
+						base: "dist",
+						keepalive: true
+					}
+				}
+			},
+			copy: {
+			  main: {
+			    files: [
+			      // includes files within path
+			      {expand: true, 
+			       src: ['css/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['js/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['images/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['font/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['people/images/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['pdfs/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},
+			       {expand: true, 
+			       src: ['downloads/**'], 
+			       cwd: 'src/',
+			       dest: 'dest/'},			    ],
+			  },
+			},
+
+
+});
+
+	// DEFAULT TASKS
+	grunt.registerTask('default', ['sass', 'jade', 'cssmin', 'copy']);
+	grunt.registerTask('build', ['sass', 'jade', 'cssmin', 'clean']);
+
+	grunt.loadNpmTasks('grunt-replace');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+};
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# limlab-website
+![alt tag](https://raw.github.com/dogfalo/materialize/master/images/materialize.gif)
+===========
+
+[![Gitter](https://badges.gitter.im/Join Chat.svg)](https://gitter.im/Dogfalo/materialize?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+Materialize, a CSS Framework based on material design
+
+### Current Version : v0.92.1
+
+##Requirements:
+- Ruby Sass 4.3+, LibSass 0.6+
+
+##Supported Browsers:
+Chrome 35+, Firefox 31+, Safari 7+, IE 10+
+
+##Changelog
+
+- v0.92.1
+  - Bower semver fix
+  - Added new radio button style
+
+- v0.92.0
+  - Clicking icon in dropdown in navbar no longer closes dropdown immediately
+  - Multiple select inputs now work properly
+  - Mobile navbar no longer extends past screen width
+  - Parallax improved
+  - Modal restructured / can be opened programmatically
+  - Callbacks added to modals
+  - Added dist folder to repo
+  - Cards restructured
+
+
+- v0.91
+  - bug fixes to forms
+  - added waves color classes
+  - toast thickened to look better on mobile
+  - many other bug fixes
+
+
+- v0.9
+  - Touch interactions added
+  - tons more...
