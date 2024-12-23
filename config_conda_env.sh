2b18f3f0c0c38b4f597507acedfa75dfbc34cfcb
Hersh Bhargava <hershkbhargava@gmail.com>
2024-12-23 10:14:41 -0800

    prog

--- config_conda_env.sh
+++ config_conda_env.sh
@@ -13,9 +13,10 @@ conda env create -f environment.yml -v
 # Activate the environment
 conda activate limlab-website 
 
-gem install sass
+# gem install sass
 
 npm install
+npm install -g sass
 # npm install grunt-contrib-sass --save-dev
 # npm install -g grunt-cli@1.5.0
 # npm install grunt@0.4.5 --save-dev
