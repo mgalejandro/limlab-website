430a04a9ce423c372db848aab55702ba97946bd1
Hersh Bhargava <hershkbhargava@gmail.com>
2024-12-23 11:21:09 -0800

    prog

--- config_conda_env.sh
+++ config_conda_env.sh
@@ -4,8 +4,8 @@
 eval "$(conda shell.bash hook)"
 
 # Remove env if needed
-conda deactivate
-conda env remove -n limlab-website
+# conda deactivate
+# conda env remove -n limlab-website
 
 # Create the Conda environment
 conda env create -f environment.yml -v
