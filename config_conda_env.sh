8ec987a798bf28db92f9275c9a95491866d00884
Hersh Bhargava <hershkbhargava@gmail.com>
2024-12-23 09:43:09 -0800

    conda implementation

---
+++ config_conda_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+
+# init conda
+eval "$(conda shell.bash hook)"
+
+# Create the Conda environment
+conda env create -f environment.yml --force -v
+
+# Activate the environment
+conda activate limlab-website 
+
+npm install -g grunt-cli@1.5.0
